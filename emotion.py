a30e7137fa5277189d6265d00a2aed691ebec5c1
Rashad Garayev <garayevrashad@hotmail.com>
2019-04-16 18:32:21 +0400

    update

--- emotion.py
+++ emotion.py
@@ -1,5 +1,3 @@
-
-
 import sys,os,cv2,time
 from keras.preprocessing.image import img_to_array
 from keras.models import load_model
@@ -31,82 +29,100 @@ class Window(QMainWindow):
         self.timer.start(4)
 
     def update_frame(self):
-        
+        ret, self.image = self.capture.read()
         faceCascade = cv2.CascadeClassifier(
             'Cascade/haarcascade-frontalface-default.xml')
         font = cv2.FONT_HERSHEY_SIMPLEX
-
+        
         #Dictionary for emotion recognition model output and emotions
         emotions = {0: 'Angry', 1: 'Fear', 2: 'Happy',
                     3: 'Sad', 4: 'Surprised', 5: 'Neutral'}
 
-        
+        #Upload images of emojis from emojis folder
         emoji = []
         for index in range(6):
             emotion = emotions[index]
             emoji.append(cv2.imread('emoji/' + emotion + '.png', -1))
         model = load_model('models/emotion.h5', compile=False)
         frame_count = 0
-        ret, self.image = self.capture.read()
+        
+
         if ret:
-            gray = cv2.cvtColor(self.image, cv2.COLOR_BGR2GRAY)
-            faces = faceCascade.detectMultiScale(gray,scaleFactor=1.1,minNeighbors=7,minSize=(100, 100),)
             
+            gray = cv2.cvtColor(self.image, cv2.COLOR_BGR2GRAY)
+
+            #Face detection takes approx 0.07 seconds
+            start_time = time.time()
+            faces = faceCascade.detectMultiScale(
+                gray,
+                scaleFactor=1.1,
+                minNeighbors=7,
+                minSize=(100, 100),)
+            
+            y0 = 15
+            for index in range(6):
+                emotions[index] + ': ', (5, y0)
+                y0 += 15
+
             try:
-                
-                
+                # Flag for showing probability graph of only one face
+                FIRSTFACE = True
                 if len(faces) > 0:
-                    for x, y, w, h in faces:
-                        cropped_face = gray[y:y + h, x:x + w]
+                    for x, y, width, height in faces:
+                        cropped_face = gray[y:y + height, x:x + width]
                         test_image = cv2.resize(cropped_face, (48, 48))
                         test_image = test_image.reshape([-1, 48, 48, 1])
+
                         test_image = np.multiply(test_image, 1.0 / 255.0)
+
                         # Probablities of all classes
                         #Finding class probability takes approx 0.05 seconds
                         start_time = time.time()
                         if frame_count % 5 == 0:
                             probab = model.predict(test_image)[0] * 100
-                            #print("--- %s seconds ---" % (time.time() - start_time))
-                            #Finding label from probabilities
-                            #Class having highest probability considered output label
+                            
                             label = np.argmax(probab)
                             probab_predicted = int(probab[label])
                             predicted_emotion = emotions[label]
+                            
                             frame_count = 0
-                            self.listView.adItems(predicted_emotion)
+                            
+                           
+
                         frame_count += 1
-                    
-                   
-                    
+                       
                         # Drawing on frame
-                        font_size = w / 300
-                        filled_rect_ht = int(h / 5)
+                        font_size = width / 300
+                        filled_rect_ht = int(height / 5)
+
                         #Resizing emoji according to size of detected face
                         emoji_face = emoji[(label)]
-                        emoji_face = cv2.resize(emoji_face, (filled_rect_ht, filled_rect_ht))
+                        emoji_face = cv2.resize(
+                            emoji_face, (filled_rect_ht, filled_rect_ht))
+
                         #Positioning emojis on frame
-                        emoji_x1 = x + w - filled_rect_ht
+                        emoji_x1 = x + width - filled_rect_ht
                         emoji_x2 = emoji_x1 + filled_rect_ht
-                        emoji_y1 = y + h
+                        emoji_y1 = y + height
                         emoji_y2 = emoji_y1 + filled_rect_ht
+
                         #Drawing rectangle and showing output values on frame
-                        cv2.rectangle(self.image, (x, y), (x + w,
-                                                      y + h), (155, 155, 0), 2)
-                        cv2.rectangle(self.image, (x-1, y+h), (x+1 + w, y + h+filled_rect_ht),
+                        cv2.rectangle(self.image, (x, y), (x + width,
+                                                      y + height), (155, 155, 0), 2)
+                        cv2.rectangle(self.image, (x-1, y+height), (x+1 + width, y + height+filled_rect_ht),
                                       (155, 155, 0), cv2.FILLED)
                         cv2.putText(self.image, predicted_emotion+' ' + str(probab_predicted)+'%',
-                                    (x, y + h + filled_rect_ht-10), font, font_size, (255, 255, 255), 1, cv2.LINE_AA)
+                                    (x, y + height + filled_rect_ht-10), font, font_size, (255, 255, 255), 1, cv2.LINE_AA)
                         
                         # Showing emoji on frame
                         for c in range(0, 3):
-                            self.image[emoji_y1:emoji_y2, emoji_x1:emoji_x2, c] = emoji_face[:, :, c] * \
-                                (emoji_face[:, :, 3] / 255.0) + self.image[emoji_y1:emoji_y2, emoji_x1:emoji_x2, c] * \
-                                (1.0 - emoji_face[:, :, 3] / 255.0)
-                        
+                            self.image[emoji_y1:emoji_y2, emoji_x1:emoji_x2, c] = emoji_face[:, :, c] * (emoji_face[:, :, 3] / 255.0) + self.image[emoji_y1:emoji_y2, emoji_x1:emoji_x2, c] * (1.0 - emoji_face[:, :, 3] / 255.0)
+                            
             except Exception as error:
+
                 pass
         
-
+        
         self.displayImage(self.image, 1)
 
     def displayImage(self, img, windows=1):
